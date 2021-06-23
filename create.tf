33811a002828b37d8a5933a155ea486fd17dacff
Brad Sickles <brad.sickles@gmail.com>
2021-06-23 19:53:56 -0400

    Updating lambda invocations to meet impl

--- create.tf
+++ create.tf
@@ -1,29 +1,25 @@
 data "aws_lambda_invocation" "create-user" {
   function_name = data.ns_connection.postgres.outputs.lambda_function_name
 
-  input = jsonencode({
+  input = sensitive(jsonencode({
     type = "create-user"
     metadata = {
-      role_name = local.username
+      username = local.username
+      password = random_password.this.result
     }
-  })
-}
-
-locals {
-  create_user_result  = jsondecode(data.aws_lambda_invocation.create_user.result)
-  password_secret_arn = local.create_user_result["password_secret_arn"]
+  }))
 }
 
 data "aws_lambda_invocation" "create-database" {
-  function_name = data.ns_connection.postgres.outputs.lambda_function_name
+  function_name = data.ns_connection.postgres.outputs.db_admin_function_name
 
   input = jsonencode({
     type = "create-database"
     metadata = {
-      database_name = var.database_name
-      owner         = local.username
+      databaseName = var.database_name
+      owner        = local.username
     }
   })
 
-  depends_on = [data.aws_lambda_invocation.create_user]
+  depends_on = [data.aws_lambda_invocation.create-user]
 }
