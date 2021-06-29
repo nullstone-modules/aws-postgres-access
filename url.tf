9d734451e5beb6ad2bd7c755f15d76f6e07fe2ea
Brad Sickles <brad.sickles@gmail.com>
2021-06-29 19:16:48 -0400

    url encoded postgres url in secret

--- url.tf
+++ url.tf
@@ -5,5 +5,5 @@ resource "aws_secretsmanager_secret" "url" {
 
 resource "aws_secretsmanager_secret_version" "url" {
   secret_id     = aws_secretsmanager_secret.url.id
-  secret_string = "postgres://${local.username}:${random_password.this.result}@${local.db_endpoint}/${var.database_name}"
+  secret_string = "postgres://${urlencode(local.username)}:${urlencode(random_password.this.result)}@${local.db_endpoint}/${urlencode(var.database_name)}"
 }
