dc5a221bc12c279d1dd17ffbbc73a6ae835c9402
Brad Sickles <brad.sickles@gmail.com>
2021-08-04 11:14:53 -0400

    Creating security group rules in cap

--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
+variable "app_metadata" {
+  description = <<EOF
+App Metadata is injected from the app on-the-fly.
+This contains information about resources created in the app module that are needed by the capability.
+EOF
+
+  type    = map(string)
+  default = {}
+}
+
 variable "database_name" {
   type        = string
   description = "Name of database to create in Postgres cluster"
