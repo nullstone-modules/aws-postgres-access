d1575a5c251eaf14e1f03954c0955c4655745d1a
Brad Sickles <brad.sickles@gmail.com>
2021-08-04 10:07:38 -0400

    Creating db, then user, then granting access

--- create.tf
+++ create.tf
@@ -1,16 +1,5 @@
-data "aws_lambda_invocation" "create-user" {
-  function_name = data.ns_connection.postgres.outputs.db_admin_function_name
-
-  // TODO: Wrap with sensitive when upgraded to TF 0.15
-  input = jsonencode({
-    type = "create-user"
-    metadata = {
-      username = local.username
-      password = random_password.this.result
-    }
-  })
-}
-
+// Create Database will create a database
+// Additionally, a role of the same name will be created and given "owner" over database
 data "aws_lambda_invocation" "create-database" {
   function_name = data.ns_connection.postgres.outputs.db_admin_function_name
 
@@ -18,7 +7,32 @@ data "aws_lambda_invocation" "create-database" {
     type = "create-database"
     metadata = {
       databaseName = local.database_name
-      owner        = local.username
+    }
+  })
+}
+
+data "aws_lambda_invocation" "create-user" {
+  function_name = data.ns_connection.postgres.outputs.db_admin_function_name
+
+  input = jsonencode({
+    type = "create-user"
+    metadata = {
+      username     = local.username
+      password     = random_password.this.result
+    }
+  })
+
+  depends_on = [data.aws_lambda_invocation.create-database]
+}
+
+data "aws_lambda_invocation" "create-db-access" {
+  function_name = data.ns_connection.postgres.outputs.db_admin_function_name
+
+  input = jsonencode({
+    type = "create-db-access"
+    metadata = {
+      databaseName = local.database_name
+      username     = local.username
     }
   })
 
