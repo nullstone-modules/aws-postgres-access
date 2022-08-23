59e103cc66cba798713e647f21daa7fb2ccff54b
Brad Sickles <brad.sickles@gmail.com>
2022-08-23 16:17:37 -0400

    Finalize restapi resources for pg resources

--- create.tf
+++ create.tf
@@ -7,20 +7,33 @@ provider "restapi" {
   }
 }
 
-// Create Database will create a database
-// Additionally, a role of the same name will be created and given "owner" over database
+resource "restapi_object" "database_owner" {
+  path         = "/roles"
+  id_attribute = "name"
+
+  data = jsonencode({
+    name        = local.database_name
+    useExisting = true
+    skipDestroy = true
+  })
+}
+
 resource "restapi_object" "database" {
   path         = "/databases"
   id_attribute = "name"
 
   data = jsonencode({
     name        = local.database_name
+    owner       = local.database_owner
+    useExisting = true
     skipDestroy = true
   })
+
+  depends_on = [restapi_object.database_owner]
 }
 
-resource "restapi_object" "user" {
-  path         = "/users"
+resource "restapi_object" "role" {
+  path         = "/roles"
   id_attribute = "name"
 
   data = jsonencode({
@@ -30,17 +43,52 @@ resource "restapi_object" "user" {
   })
 }
 
-resource "restapi_object" "user_access" {
-  path = "/user_access"
+resource "restapi_object" "role_member" {
+  path         = "/roles/${local.database_owner}/members/${local.username}"
+  id_attribute = "id"
 
   data = jsonencode({
-    databaseName = local.database_name
-    username     = local.username
-    skipDestroy  = true
+    member      = local.username
+    target      = local.database_owner
+    skipDestroy = true
+  })
+
+  depends_on = [
+    restapi_object.database_owner,
+    restapi_object.role
+  ]
+}
+
+resource "restapi_object" "schema_privileges" {
+  path         = "/databases/${local.database_name}/schema_privileges"
+  id_attribute = "role"
+
+  data = jsonencode({
+    database    = local.database_name
+    role        = local.username
+    skipDestroy = true
   })
 
   depends_on = [
     restapi_object.database,
-    restapi_object.user
+    restapi_object.role
+  ]
+}
+
+resource "restapi_object" "default_grants" {
+  path         = "/roles/${local.username}/default_grants"
+  id_attribute = "id"
+
+  data = jsonencode({
+    role         = local.username
+    target       = local.database_owner
+    databaseName = local.database_name
+    skipDestroy  = true
+  })
+
+  depends_on = [
+    restapi_object.role,
+    restapi_object.database,
+    restapi_object.database_owner
   ]
 }
