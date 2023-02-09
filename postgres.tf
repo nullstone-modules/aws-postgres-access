48bb88930f159992876195158898831ed32d908c
Brad Sickles <brad.sickles@gmail.com>
2023-02-09 10:13:41 -0500

    Relying on db_admin_function_url as output from postgres

--- postgres.tf
+++ postgres.tf
@@ -1,6 +1,5 @@
 data "ns_connection" "postgres" {
   name     = "postgres"
-  type     = "postgres/aws"
   contract = "datastore/aws/postgres:*"
 }
 
@@ -11,14 +10,9 @@ locals {
   db_security_group_id = data.ns_connection.postgres.outputs.db_security_group_id
 }
 
-data "aws_lambda_function_url" "db_admin" {
-  count         = local.db_admin_v5 ? 1 : 0
-  function_name = local.db_admin_func_name
-}
-
-locals {
+ locals {
   db_admin_func_name = data.ns_connection.postgres.outputs.db_admin_function_name
   db_admin_invoker   = try(data.ns_connection.postgres.outputs.db_admin_invoker, null)
   db_admin_v5        = local.db_admin_invoker != null
-  db_admin_func_url  = local.db_admin_v5 ? data.aws_lambda_function_url.db_admin[0].function_url : ""
+  db_admin_func_url  = try(data.ns_connection.postgres.outputs.db_admin_function_url, "")
 }
